7963945aa8fac44f3cdb3b676f3cb27febee9db7
Bluzzi <camilledugas.pro@gmail.com>
2023-02-05 14:40:41 +0100

    setup Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: [
+    "./app/**/*.tsx",
+    "./lib/**/*.tsx"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
